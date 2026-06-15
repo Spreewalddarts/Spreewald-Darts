8057a3d1f6596b54916018d9178667cc92efe656
Spreewalddarts <st1ve88goran@gmail.com>
2026-06-15 15:43:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-const CACHE = "spreewald-darts-v2";
-const FILES = ["./", "./index.html"];
-self.addEventListener("install", e => e.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(FILES))
-));
-self.addEventListener("fetch", e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-));
+const CACHE = "spreewald-darts-v3";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(["./", "./index.html"]))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
